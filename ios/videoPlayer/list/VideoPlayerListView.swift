9d50b4980bb5f46ed4202e008c314fa0a40925a2
entaku <entaku19890818@gmail.com>
2024-12-03 00:38:20 +0900

    add thumbnail

--- ios/videoPlayer/list/VideoPlayerListView.swift
+++ ios/videoPlayer/list/VideoPlayerListView.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  VideoPlayerListView.swift
 //  videoPlayer
 //
 //  Created by 遠藤拓弥 on 2024/12/02.
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import ComposableArchitecture
+import AVFoundation
 
 struct VideoPlayerListView: View {
     let store: StoreOf<VideoPlayerList>
@@ -68,23 +69,67 @@ struct VideoPlayerListView: View {
     }
 }
 
+
+
+struct VideoThumbnail: View {
+    let url: URL
+    @State private var thumbnail: UIImage?
+
+    var body: some View {
+        Group {
+            if let thumbnail = thumbnail {
+                Image(uiImage: thumbnail)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 80)
+                    .cornerRadius(8)
+            } else {
+                Rectangle()
+                    .fill(Color.gray.opacity(0.3))
+                    .frame(width: 120, height: 80)
+                    .cornerRadius(8)
+                    .onAppear {
+                        generateThumbnail()
+                    }
+            }
+        }
+    }
+
+    private func generateThumbnail() {
+        let asset = AVAsset(url: url)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        imageGenerator.appliesPreferredTrackTransform = true
+
+        do {
+            let cgImage = try imageGenerator.copyCGImage(at: .zero, actualTime: nil)
+            thumbnail = UIImage(cgImage: cgImage)
+        } catch {
+            print("Error generating thumbnail: \(error)")
+        }
+    }
+}
+
 struct VideoRowView: View {
     let video: VideoPlayerList.State.VideoModel
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text(video.title)
-                .font(.headline)
+        HStack {
+            VideoThumbnail(url: video.url)
 
-            Text(formatDuration(video.duration))
-                .font(.caption)
-                .foregroundColor(.secondary)
+            VStack(alignment: .leading, spacing: 4) {
+                Text(video.title)
+                    .font(.headline)
 
-            Text(formatDate(video.createdAt))
-                .font(.caption2)
-                .foregroundColor(.secondary)
+                Text(formatDuration(video.duration))
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+
+                Text(formatDate(video.createdAt))
+                    .font(.caption2)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.vertical, 4)
         }
-        .padding(.vertical, 4)
     }
 
     private func formatDuration(_ duration: Double) -> String {
